625472ccb3f908bafbf651c7eac8daa32116731f
Bhanot, Karan <k.bhanot@cvent.com>
2019-03-12 02:43:16 +0530

    Create Python scripts to collect data from Wikipedia and store in database

---
+++ Modules/WikipediaCrawler.py
@@ -0,0 +1,56 @@
+import sqlite3
+import wptools
+import re
+from bs4 import BeautifulSoup
+
+class WikipediaCrawler:
+
+    def __init__(self, db_file):
+        self.categories = []
+        self.conn = sqlite3.connect(db_file)
+        cursor = self.conn.cursor()
+        cursor.execute(
+            'CREATE TABLE IF NOT EXISTS wikiData (id text, category text, url text, content text)')
+        self.conn.commit()
+        self.cursor = self.conn.cursor()
+
+    def insert_page_content(self, id, category, url, content):
+        self.cursor.execute('INSERT INTO wikiData VALUES (?, ?, ?, ?)',
+                            (id, category, url, content))
+        self.conn.commit()
+
+    def get_ids(self):
+        return [id for id in self.cursor.execute('SELECT id FROM wikiData')]
+
+    def get_urls(self):
+        return [url for url in self.cursor.execute('SELECT url FROM wikiData')]
+
+    def collect_data(self, category, depth):
+        if depth:
+            print("Extracting data for subcategories of {} at depth {}".format(category, depth))
+            cat = wptools.category(category)
+            cat_members = cat.get_members()
+
+            if 'members' in cat_members.data.keys():
+                for cat_member in cat_members.data['members']:
+                    if cat_member['pageid'] not in self.get_ids():
+                        try:
+                            page = wptools.page(
+                                pageid=cat_member['pageid']).get_parse()
+                            url = page.get_query().data['url']
+                            text = BeautifulSoup(
+                                page.data['wikitext'], 'html.parser').get_text()
+                            clean_text = re.sub(
+                                r'\s*{.*}\s*|\s*\[.*\]\s*', '', text)
+                            print('Saving page with Id: {} and URL: {}'.format(
+                                cat_member['pageid'], url))
+                            self.insert_page_content(
+                                cat_member['pageid'], category, url, clean_text)
+                        except Exception as e:
+                            print("Exception occured: {}".format(e))
+
+            if 'subcategories' in cat_members.data.keys():
+                sub_cats = cat_members.data['subcategories']
+                for sub_cat in sub_cats:
+                    self.categories.append(sub_cat)
+                    self.collect_data(sub_cat['title'], depth - 1)
---
+++ collectData.py
@@ -0,0 +1,31 @@
+# Import libraries
+import os
+import argparse
+
+# Import modules
+from Modules.WikipediaCrawler import WikipediaCrawler
+
+# Define arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--category', dest='category')
+parser.add_argument('-d', '--depth', dest='depth')
+values = parser.parse_args()
+
+# Define global variables
+if (values.depth and values.category):
+    category = 'Category:' + str(values.category)
+    search_depth = values.depth
+else:
+    print('Error: Please enter both category and depth')
+    exit(1)
+
+# Create data directory
+if not os.path.exists('data'):
+    os.makedirs('data')
+
+# Collect data
+crawler = WikipediaCrawler('data/wikiData.db')
+crawler.collect_data(category, int(search_depth))
+urls = crawler.get_urls()
+print(urls)
+print("The database has been generated")
